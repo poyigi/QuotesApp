b2ba4ffcb81ea1c5609c4df9f0d8d6fdd6dd9626
poyigi <adepoyigiemmanuel@yahoo.com>
2022-08-01 22:46:16 -0400

    Add project files.

---
+++ Models/Quote.cs
@@ -0,0 +1,15 @@
+﻿namespace QuotesApp.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string QuoteAuthor { get; set; }
+
+        public string QuoteText { get; set; }
+
+        public Quote()
+        {
+                
+        }
+    }
+}
